43b922d377284534d6530e1211841eb1e4521e8d
vanutcho123 <hainguyen.061001@gmail.com>
2023-07-01 15:25:44 +0700

    Setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useEffect } from "react";
-import "./App.css";
-import { fetchDataFromApi } from "./utils/api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+//
+import { fetchDataFromApi } from "./utils/api";
+import Home from "./pages/home/Home";
+import "./App.scss";
+import Details from "./pages/details/details";
+import SearchResult from "./pages/searchResult/searchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/pageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,7 +23,17 @@ function App() {
       dispatch(getApiConfiguration(res));
     });
   };
-  return <div className="App">{url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
