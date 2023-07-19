deb47ba532eb8173b5f393b9bf5de81f6ef2d498
vanutcho123 <hainguyen.061001@gmail.com>
2023-07-19 16:55:11 +0700

    CAROUSEL SECTION , GENRES COMPONENT

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
-//
 import { fetchDataFromApi } from "./utils/api";
-import Home from "./pages/home/Home";
-import "./App.scss";
-import Details from "./pages/details/details";
-import SearchResult from "./pages/searchResult/searchResult";
-import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/pageNotFound";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, []);
 
   const fetchApiConfig = () => {
@@ -32,6 +36,25 @@ function App() {
       dispatch(getApiConfiguration(url));
     });
   };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
   return (
     <BrowserRouter>
       <Header />
@@ -40,7 +63,7 @@ function App() {
         <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="/*" element={<PageNotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,14 @@
 import React from "react";
 import "./Home.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
+      <div className="" style={{ height: 1000 }}></div>
     </div>
   );
 };
